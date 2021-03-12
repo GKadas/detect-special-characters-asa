a5c4a63fc2349cfe2aaf551b98d4f964e890f023
George Kadas <kadas.george@yahoo.com>
2021-03-12 23:57:45 +0100

    basic GUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import {Typography, AppBar, TextareaAutosize, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core';
+import BeenhereSharpIcon from '@material-ui/icons/BeenhereSharp';
+import useStyles from './styles';
 
 function App() {
+
+  const classes = useStyles();
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <BeenhereSharpIcon className={classes.icon}/>
+          <Typography variant="h6">ASA Running Config Parser</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="lg" justify="center" className={classes.containedText}>
+            <TextareaAutosize
+              className={classes.textArea}
+              aria-label="minimum height"
+              rowsMin={20}
+              placeholder="Enter ASA show running-configuration output"
+            />
+          </Container>
+          <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,47 @@
+import {makeStyles, ThemeProvider} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  containedText: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '10px'
+
+  },
+  icon: {
+    marginRight: '20px',
+  },
+  textArea: {
+    width: '600px',
+	  height: '120px',
+	  border: '3px solid #cccccc',
+	  padding: '5px'
+  },
+  button: {
+    marginTop: '40px'
+  },
+  cardGrid: {
+    padding: '20px 0'
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+
+  },
+  cardMedia: {
+    paddingTop: '56.25%' //16:9 ratio
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: '50px 0'
+  }
+}));
+
+export default useStyles;
